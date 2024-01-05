d0f465f40c5c7af54b42bb7126bc95be737bc547
Ordasto <34802073+Ordasto@users.noreply.github.com>
2024-01-05 13:39:12 +0000

    first commit

---
+++ include/runic_c.h
@@ -0,0 +1,16 @@
+
+
+#define ᚺ unsigned
+#define ᛰ void
+#define ᚬ int
+#define ᚳ long
+
+#define ᚫ float
+#define ᚦ double
+
+#define ᛕ char
+
+#define ᚱ return
+
+#define ᛪ /
+#define ᛤ *
---
+++ src/main.c
@@ -0,0 +1,11 @@
+
+#include <stdio.h>
+
+#include <runic_c.h>
+
+
+ᚬ main(ᛰ) {
+  ᚬ val = 10;
+  printf("Hello World, %d n", val);
+  ᚱ 0;
+}
